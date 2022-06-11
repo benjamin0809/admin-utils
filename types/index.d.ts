3ea5ecd8df6b6d190f4424cd55656b93ca1d3446
zhaoguoxiang <zhaoguoxiang@aspirecn.com>
2022-06-12 00:58:01 +0800

    update types

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,7 +2,7 @@
  * 深拷贝
  * @return new
  */
-export declare const deepClone: (obj: Array<unknown> | object) => any;
+export declare const deepClone: (obj: Array<unknown> | Record<string, any>) => any;
 
 export declare const throttle: (fn: throttleFn, delay: number | undefined, context: any, ...args: any) => (...fnArgs: any) => void;
 
